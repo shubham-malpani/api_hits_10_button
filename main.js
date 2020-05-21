be362c940013886a837e7b82a1df1cd2deface4b
Shubham Malpani <51014963+shubham-malpani@users.noreply.github.com>
2020-05-21 11:28:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-// import _ from "lodash";
-//import data from './data.json'
-const url = "http://127.0.0.1:8000/poll/camfeed/";
+const url = "http://api.com/";
 
 let data = {
   "cam1" : {
@@ -12,46 +10,6 @@ let data = {
       "camera_id": 2,
       "person_id": "122",
       "is_present": false
-  },
-  "cam3" : {
-      "camera_id": 3,
-      "person_id": "123",
-      "is_present": true
-  },
-  "cam4" : {
-      "camera_id": 4,
-      "person_id": "124",
-      "is_present": true
-  },
-  "cam5" : {
-      "camera_id": 5,
-      "person_id": "125",
-      "is_present": false
-  },
-  "cam6" : {
-      "camera_id": 6,
-      "person_id": "126",
-      "is_present": true
-  },
-  "cam7" : {
-      "camera_id": 7,
-      "person_id": "127",
-      "is_present": true
-  },
-  "cam8" : {
-      "camera_id": 8,
-      "person_id": "128",
-      "is_present": false
-  },
-  "cam9" : {
-      "camera_id": 9,
-      "person_id": "129",
-      "is_present": true
-  },
-  "cam10" : {
-      "camera_id": 10,
-      "person_id": "130",
-      "is_present": false
   }
 }
 
@@ -61,11 +19,7 @@ function onbtnClick(event){
     let data_config =  data[event.id]
 
     let request = {
-        "camera_details": "face_exists",
-        "message": "face found",
-        "face_cordinate": "[]",
-        "match_score": "Matched",
-        "person_image_url": ""
+        //payload
     }
 
     request = Object.assign(request, data_config);
